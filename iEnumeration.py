3642f5232816eeca5429f1ff217594c74dbfb50c
DerTrigger <34043508+DerTrigger@users.noreply.github.com>
2018-05-01 11:11:23 -0500

    Add files via upload
    Implicit enumeration code, plus script with 2 examples to test it out.

--- iEnumeration.py
+++ iEnumeration.py
@@ -1,94 +1,4 @@
 
-"""
-
-@author: Guillermo Flores
-e-mail: gflores10.1926@gmail.com
-"""
-''' ------------------------------------------EXAMPLE  1------------------------------------'''
-'''
-rD = {} #Empty dictionary where we will store restrictions (restrictions Dictionary)
-rD [1]= ['a','e']
-rD [2] = ['b','c']
-rD [3] = ['c','d']
-rD [4] = ['e','d']
-rD [5] = ['a','b','d']
-rD [6] = ['b','h','i']
-rD [7] = ['a','h','i']
-rD [8] = ['d','f','i']
-rD [9] = ['f','g','h']
-rD [10] = ['d','h','i']
-#We could read user imputed values in a while loop and store them in the    
-#    dictionary.
-totVar = 9 #ammount of variables
-totRestric = 10 #ammount of bilateral relations between variables
-totRooms = 2 #ammount of available rooms
-ren, col = totVar + 1, totVar
-cost = [[0 for x in range(col)] for y in range (ren)]
-var = {}
-var ['a'] = []
-var ['b'] = []
-var ['c'] = []
-var ['d'] = []
-var ['e'] = []
-var ['f'] = []
-var ['g'] = []
-var ['h'] = []
-var ['i'] = []
-
-#Start conditions, group restrictions
-sC = {}
-sC ['a'] = 3
-sC ['b'] = 2
-'''
-'''------------------------END OF EXAMPLE 1 ---------------------------------'''
-
-'''----------------------- EXAMPLE 2 ----------------------------------------'''
-
-#List of needed variables
-rD = {} #restrictions Dictionary
-rD [1] = ['i','g']
-rD [2] = ['a','c','e','g']
-rD [3] = ['b','a','g']
-rD [4] = ['e','g']
-rD [5] = ['i','d','j','k']
-rD [6] = ['m','i','d']
-rD [7] = ['f','d','k']
-rD [8] = ['a','f','l','k']
-rD [9] = ['b','m']
-rD [10] = ['g','k']
-rD [11] = ['a','k','l','i']
-rD [12] = ['a','h','l']
-rD [13] = ['c','n','h','m']
-rD [14] = ['c','b']
-rD [15] = ['j','h']
-var = {} #relations of letter Dictionary
-var['a'] = []
-var['b'] = []
-var['c'] = []
-var['d'] = []
-var['e'] = []
-var['f'] = []
-var['g'] = []
-var['h'] = []
-var['i'] = []
-var['j'] = []
-var['k'] = []
-var['l'] = []
-var['m'] = []
-var['n'] = []
-sC = {} #start conditions Dictionary
-sC['a'] = 3
-sC['c'] = 3
-sC['d'] = 3
-sC['i'] = 4
-totVar = 14 #ammount of variables
-totRestric = 15 #ammount of bilateral relations between variables
-totRooms = 2 #ammount of available rooms
-ren, col = totVar + 1, totVar
-cost = [[0 for x in range(col)] for y in range (ren)]
-         
-
-'''-----------------------END OF EXAMPLE 2 -----------------------------------'''
 #get relations of every course
 def relations(): 
     for i in range (0, len(var)) : #for every variable
@@ -249,14 +159,87 @@ def assignGroups(actualRow):
     #end while        
 #end assignGroups
 
+def createVar(total):
+    for i in range (0, total): 
+        var[chr(97+i)] = []
+#end createVar
 def runMaster():
     relations()
     fillCosts()
     addInitialRestrictions(sC)
     assignGroups(0)
-    print("Results are stored in 'groups' and in 'cost'")
+    print("Results are stored in 'groups' and in 'final_cost'")
 #end runMaster
+
+''' ------------------------- GUI --------------------------------------'''
+'''RAN OUT OF TIME, SORRY '''
+#List of needed variables
+rD = {} #restrictions Dictionary
+var = {} #relations of letter Dictionary
+sC = {} #start conditions Dictionary
+totVar = 0 #ammount of variables 14
+totRestric = 0 #ammount of bilateral relations between variables 15
+totRooms = 0 #ammount of available rooms 2
+
+x = int(input("Give me the number of variables to be used: "))
+totVar = x
+createVar(x)
+ren, col = totVar + 1, totVar
+cost = [[0 for x in range(col)] for y in range (ren)]
+ 
+y = int(input("Give me the number of relations to be used: "))
+totRestric = y 
+'''
+rD [1] = ['i','g']
+rD [2] = ['a','c','e','g']
+rD [3] = ['b','a','g']
+rD [4] = ['e','g']
+rD [5] = ['i','d','j','k']
+rD [6] = ['m','i','d']
+rD [7] = ['f','d','k']
+rD [8] = ['a','f','l','k']
+rD [9] = ['b','m']
+rD [10] = ['g','k']
+rD [11] = ['a','k','l','i']
+rD [12] = ['a','h','l']
+rD [13] = ['c','n','h','m']
+rD [14] = ['c','b']
+rD [15] = ['j','h']
+'''
+for i in range(0, y):
+    print("Give me relation ", i+1)
+    r = input()
+    rD[i+1] = r.split()
+    
+z = int(input("Give me the number of group initial restrictions: "))
+for i in range(0, z):
+    letter = input("Give me course to restrict: ")
+    greater = int(input("must be greater than: "))
+    sC[letter] = greater
+
+w = int(input("Give me the number of available rooms: "))
+totRooms = w
+'''sC['a'] = 3
+sC['c'] = 3
+sC['d'] = 3
+sC['i'] = 4'''
+print("")
+print("Number of variables used: ", x)
+print("Number of relations used: ", y)
+print("Number of group initial restrictions used: ", z)
+print("Available rooms: ", w)
+print("")
 
 runMaster()
 
+''' PLOT THE RESULTING GROUPS AND COST TABLE '''
+import numpy as np
+import pandas as pd
+final_cost = np.matrix(cost)
+for i in range(0, len(groups)):
+    while(len(groups[i+1])<totRooms):
+        groups[i+1].append('-')
         
+final_groups = pd.DataFrame(groups) 
+print(final_cost)
+#print(final_groups)
